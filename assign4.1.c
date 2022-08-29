d16b56891632d0b4cce5a75734079e914212363a
Sadik Khatik <104684326+thesadikkhatik@users.noreply.github.com>
2022-08-29 21:58:31 +0530

    main

---
+++ assign4.1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=0; i<=5; i++)
+        printf("\nMySirG");
+    return 0;
+}
---
+++ assign4.7.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=20;i>=0;i=i-2)
+        printf("\t%d",i);
+}
